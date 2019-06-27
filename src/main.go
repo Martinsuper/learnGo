1293dedc560d7abc8b172e68ab5eece5b2fc9426
Martin <mrtduan@gmail.com>
2019-06-27 20:14:45 +0800

    修改main入口函数

--- src/main.go
+++ src/main.go
@@ -1,22 +1,20 @@
 package main
 
-import "mAlgorithm/aMoveImage"
+import (
+	"fmt"
+	"mAlgorithm/aSort"
+	"mAlgorithm/getData"
+	"time"
+)
 
 func main() {
-	//var findImagePath string
-	//var saveImagePath string
-	//flag.StringVar(&findImagePath, "f", "./", "输入要移动图片的目录")
-	//flag.StringVar(&saveImagePath, "t", "./", "输入要保存图片的路径")
-	//flag.Parse()
-	aMoveImage.HandleImage("/Users/duanyu/Desktop/image1/image2other", "/Users/duanyu/Desktop/image")
+	bubbleTime()
+}
 
-	//ans := aMoveImage.GetImagePath("/Users/duanyu/workspace/learnGo/src/imagelist.json")
-	//for i := 0; i < len(ans); i++{
-	//	fmt.Println(ans[i])
-	//}
-	//
-	//ans := aMoveImage.ReadImage("/Users/duanyu/Desktop/cmb_yearbill1548510092673.jpg")
-
-	//
-	//fmt.Println(ans)
+func bubbleTime() {
+	ans := getData.ReadFile("/Users/duanyu/workspace/learnGo/src/mAlgorithm/data/sortData1")
+	start := time.Now()
+	aSort.Sbubble(ans)
+	end := time.Since(start)
+	fmt.Println(end)
 }
